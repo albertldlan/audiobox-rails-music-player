8349b65d8fe16a99f131eaaa4f591bd0963626a4
albertldlan <albertldlan@gmail.com>
2013-07-22 17:24:14 -0700

    fix empty comment displayed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-  	@comment = current_user.comments.create(params[:comment])
+  	@comment = current_user.comments.create!(params[:comment])
     @song = Song.find(params[:song_id])
     @comments = @song.comments
     respond_to do |format|
   		format.html
   		format.js
-  	end
+    end
   end
 
   def index
